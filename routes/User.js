4cfaf155c6502a128f6fc1ae523610f70a90c608
kevin <kevinmt191919@gmail.com>
2020-09-07 23:53:05 -0700

    Fixes

--- routes/User.js
+++ routes/User.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 //DESTRUCTURING METHODS AND FUNCTIONS
 //
-const { requireSignin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 //
 const { userById } = require('../controllers/user');
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, (req, res) => {
   res.json({
     user: req.profile,
   });
 });
 
 //execute everytime
-//anytime there is a parameter called 'userId' in the route execute the method userById
+//anytime there is a parameter called 'userId' in the route execute the method userById, it cna be ANY route
 //this middleware will makes sure to set the req --> req.profile to the user data sent back from DB
 router.param('userId', userById);
 
