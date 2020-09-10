86dccf6bb256104450faa44e2e5b19756b359c20
kevin <kevinmt191919@gmail.com>
2020-09-09 23:54:53 -0700

    outputting data

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,20 @@
 const Category = require('../models/category');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  //create a new object of category from the Category Model
+  const category = new Category(req.body);
+
+  //have to save the changes to the database
+  category.save((err, data) => {
+    //if there are any erros
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      data,
+    });
+  });
+};
