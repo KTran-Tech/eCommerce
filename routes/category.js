56311ddb11a9c6409d6d6561acbe1f972af32c3b
kevin <kevinmt191919@gmail.com>
2020-09-10 23:52:36 -0700

    UserbyId

--- routes/category.js
+++ routes/category.js
@@ -4,8 +4,13 @@ const router = express.Router();
 //==========================================
 const { create } = require('../controllers/category');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
-
+const { userById } = require('../controllers/user');
 //==========================================
+
+//execute everytime
+//anytime there is a parameter called 'userId' in the route execute the method userById, it cna be ANY route
+//this middleware will makes sure to set the req --> req.profile to the user data sent back from DB
+router.param('userId', userById);
 
 router.get('/create/:userId', requireSignin, isAuth, isAdmin, create);
 
