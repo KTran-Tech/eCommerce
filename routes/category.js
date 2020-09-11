d0d7821577f2c70123b1f603832a264600f11a8b
kevin <kevinmt191919@gmail.com>
2020-09-10 23:44:37 -0700

    import in middleware

--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,8 @@ const router = express.Router();
 //DESTRUCTURING METHODS AND FUNCTIONS
 //==========================================
 const { create } = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+
 //==========================================
 
 router.get('/create', create);
