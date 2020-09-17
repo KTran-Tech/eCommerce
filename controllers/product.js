7dc3a85cc20908a69605e7de35b9c42098eec7bf
kevin <kevinmt191919@gmail.com>
2020-09-16 23:58:26 -0700

    Finished Middleware

--- controllers/product.js
+++ controllers/product.js
@@ -11,8 +11,12 @@ const { errorHandler } = require('../helpers/dbErrorHandler');
 exports.productById = (req,res,next,id) =>{
   Product.findById(id).exec((err,product)=>{
     if(err||!product){
-      
+      return res.status(400).json({
+        error: "Product not found"
+      })
     }
+    req.product = product;
+    next();
   })
 }
 
