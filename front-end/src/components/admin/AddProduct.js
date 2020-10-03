3ef483dcca5e9f8bc514ea027e1886811c27f91c
Kevin Tran <kevinmt191919@gmail.com>
2020-10-02 17:25:45 -0700

    Form Data

--- front-end/src/components/admin/AddProduct.js
+++ front-end/src/components/admin/AddProduct.js
@@ -37,15 +37,34 @@ const AddProduct = () => {
     formData,
   } = values;
 
+  //here we are making use of the available FormData in the browser, make it available as soon as the component mounts
+  //everytimes the page loads useEffects runs once because of the '[]'
+  useEffect(() => {
+    setValues({ ...values, formData: new FormData() });
+  }, []);
+
+  const handleChange = (e) => {
+    //if the name is not 'photo' then set 'value' to the event's value else assign to its first array file
+    const value =
+      e.target.name === 'photo' ? e.target.files[0] : e.target.value;
+    //the form data is what we use to send back to the backend
+    formData.set(e.target.name, value);
+    setValues({ ...values, [e.target.name]: value });
+  };
+
+  const clickSubmit = (e) => {
+    //
+  };
+
   const newPostform = () => (
-    <form className='mb-3'>
+    <form className='mb-3' onSubmit={clickSubmit}>
       {/*        */}
-      <h4>Post Photo</h4>
+      <h4>Post photo</h4>
 
       <div className='form-group'>
         <label className='btn btn-secondary'>
           <input
-            onChange={handleChange('photo')}
+            onChange={(e) => handleChange(e)}
             type='file'
             name='photo'
             accept='image/*'
@@ -56,12 +75,58 @@ const AddProduct = () => {
       <div className='form-group'>
         <label className='text-muted'>Name</label>
         <input
-          onChange={handleChange('name')}
+          onChange={(e) => handleChange(e)}
           type='text'
           className='form-control'
           value={name}
         />
       </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Description</label>
+        <textarea
+          onChange={(e) => handleChange(e)}
+          className='form-control'
+          value={description}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Price</label>
+        <input
+          onChange={(e) => handleChange(e)}
+          type='text'
+          className='form-control'
+          value={price}
+        />
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Category</label>
+        <select onChange={(e) => handleChange(e)} className='form-control'>
+          <option value='5f758518ebcea61bfd10cd02'>NodeJs</option>
+        </select>
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Shipping</label>
+        <select onChange={(e) => handleChange(e)} className='form-control'>
+          <option value='0'>No</option>
+          <option value='1'>Yes</option>
+        </select>
+      </div>
+
+      <div className='form-group'>
+        <label className='text-muted'>Quantity</label>
+        <input
+          onChange={(e) => handleChange(e)}
+          type='number'
+          className='form-control'
+          value={quantity}
+        />
+      </div>
+
+      <button className='btn btn-outline-primary'>Create Product</button>
       {/*        */}
     </form>
   );
