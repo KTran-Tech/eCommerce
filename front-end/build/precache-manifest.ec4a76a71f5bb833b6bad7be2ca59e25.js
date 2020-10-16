2554f81c17838ca9e0174a4a3fc34455459fea9b
Kevin Tran <kevinmt191919@gmail.com>
2020-10-16 15:40:59 -0700

    Build

---
+++ front-end/build/precache-manifest.ec4a76a71f5bb833b6bad7be2ca59e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d028f538dd8acad861fbc61e3ad5bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a1c6537e411054435f",
+    "url": "/static/css/main.1e2e5658.chunk.css"
+  },
+  {
+    "revision": "88b6993abb3d84d10db4",
+    "url": "/static/js/2.152fa1d6.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.152fa1d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a1c6537e411054435f",
+    "url": "/static/js/main.b25945d3.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
