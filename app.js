07a60df5b8a76135448a3496ca66030463146eda
Kevin Tran <kevinmt191919@gmail.com>
2020-10-16 22:30:21 -0700

    make it better

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 //default is 8000 but process.env.port is in production
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
